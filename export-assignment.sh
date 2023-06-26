e5c40537ef42d3fbe42b45cff73faa245e4e0984
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2023-06-25 22:22:35 -0300

    Reorganização

---
+++ export-assignment.sh
@@ -0,0 +1,4 @@
+make clean
+rmdir bin
+cd ..
+tar -zcvf Grupo_H.tar.gz INE5412-OS/assets/* INE5412-OS/docs/* INE5412-OS/include/* INE5412-OS/os/* INE5412-OS/src/* INE5412-OS/makefile INE5412-OS/README.md
--- export.sh
+++
@@ -1,4 +0,0 @@
-make clean
-rmdir bin
-cd ..
-zip Grupo_H.zip -r INE5412-OS/src/*.{h,cc} INE5412-OS/makefile INE5412-OS/README.md
